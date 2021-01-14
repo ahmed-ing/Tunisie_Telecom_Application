6d728d85e62b899667c0999cc1d0a9bd710d0b95
Ahmed Bentouila <ahmed.bentouila1@esprit.tn>
2021-01-14 14:55:26 +0100

    first commit

---
+++ test2/deplacement/urls.py
@@ -0,0 +1,16 @@
+
+from django.conf.urls import url
+from . import views
+app_name='deplacement'
+
+
+
+
+urlpatterns = [
+   
+    url(r'^$', views.liste, name='liste'),
+    url(r'^nouveau$', views.nouveau, name='nouveau'),
+      url(r'^(?P<code>\w{6})/$', views.redirection, name='url_redirection'),
+    
+   
+]
